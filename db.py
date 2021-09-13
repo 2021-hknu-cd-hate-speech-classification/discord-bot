ed08718df412718f8a3920cbe6a572ce6e6cb0d5
vallisneria <moogi3469@gmail.com>
2021-09-13 16:13:18 +0900

    악플 작성 횟수 기록 기능 관련 코드 수정
    - 악플로 판단되는 채팅 작성자, 작성된 서버 이름, 채팅 내용, 점수를 기록하는 기능 추가
    - 기록된 내용을 조회하는 기능 추가

--- db.py
+++ db.py
@@ -10,5 +10,27 @@ class EosaDatabase:
                                  port=os.environ["POSTGRES_PORT"],
                                  database="eosa")
 
-    def user_append(self, guild_id: int, user_id: int):
-        raise NotImplementedError
+        cursor = self.__conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS detect_log ("
+                       "    user_id INTEGER,"
+                       "    guild_id INTEGER,"
+                       "    chat_txt TEXT,"
+                       "    score REAL"
+                       ")")
+        cursor.commit()
+
+    def add_detect_log(self, user_id: int, guild_id: int, txt: str, score: float):
+        cursor = self.__conn.cursor()
+        cursor.execute("INSERT INTO detect_log(user_id, guild_id, chat_txt, score) VALUES (%s, %s, %s, %s)",
+                       (user_id, guild_id, txt, score))
+        cursor.commit()
+
+    def get_guild_detected_log(self, guild_id: int) -> str:
+        cursor = self.__conn.cursor()
+        cursor.execute("SELECT user_id, chat_txt, score FROM detect_log WHERE guild_id=%s", (guild_id,))
+        result = ""
+        for row in cursor:
+            user_id, txt, score = row
+            result += f"{user_id}: {txt} ({score})\n"
+
+        return result
--- discord_bot.py
+++ discord_bot.py
@@ -1,17 +1,19 @@
 import os
-import util
 import discord
 import torch
 from hate_speech_classification_model import HateSpeechClassifier
 
-# https://discord.com/api/oauth2/authorize?client_id=823899168942456922&permissions=8258&scope=bot
+import util
+import db
 
+# https://discord.com/api/oauth2/authorize?client_id=823899168942456922&permissions=8258&scope=bot
 
 client = discord.Client()
 token = os.environ["DISCORD_TOKEN"]
 
 MODEL_PATH = os.environ["MODEL_PATH"]
 model: HateSpeechClassifier = torch.load(MODEL_PATH, map_location="cpu")
+eosa_db = db.EosaDatabase()
 
 
 @client.event
@@ -28,6 +30,10 @@ async def on_message(message):
         await message.add_reaction("⏭️")
         return None
 
+    if message.content.startswith("?"):
+        log = eosa_db.get_guild_detected_log(message.guild.id)
+        await message.reply(log)
+
     if len(message.content) >= 5:
         cleaned_msg = util.clean_discord_markdown(message.content)
         score = model.infer(cleaned_msg)
@@ -35,6 +41,7 @@ async def on_message(message):
         await message.add_reaction(f"{int(score[1] * 10)}\uFE0F\u20E3")
 
         if score[1] >= 0.7:
+            eosa_db.add_detect_log(message.author.id, message.guild.id, cleaned_msg, score[1])
             await message.add_reaction("🤬")
 
     else:
