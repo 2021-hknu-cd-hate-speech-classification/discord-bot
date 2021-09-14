98432eb05749a0ae567cdc3e121fdcf97fb298ea
vallisneria <moogi3469@gmail.com>
2021-09-14 17:57:08 +0900

    `EosaDatabase` 클래스 수정
    - `EosaDatabase.get_guild_detected_log` 메서드의 반환 결과가 읽는 방법도 같이 반환하도록 변경
    - 유저의 적발 횟수를 반환하는 `EosaDatabase.get_user_detection_count` 함수 작성

--- db.py
+++ db.py
@@ -27,9 +27,16 @@ class EosaDatabase:
     def get_guild_detected_log(self, guild_id: int) -> str:
         cursor = self.__conn.cursor()
         cursor.execute("SELECT user_id, chat_txt, score FROM detect_log WHERE guild_id=%s", (guild_id,))
-        result = ""
+        result = "유저 ID: 텍스트 (점수)\n============================="
         for row in cursor:
             user_id, txt, score = row
             result += f"{user_id}: {txt} ({score})\n"
 
         return result
+
+    def get_user_detected_count(self, user_id: int, guild_id: int) -> str:
+        cursor = self.__conn.cursor()
+        cursor.execute("SELECT COUNT(*) FROM detect_log WHERE user_id=%s AND guild_id=%s",
+                       (user_id, guild_id))
+
+        return f"{cursor.fetchone()[0]}"
